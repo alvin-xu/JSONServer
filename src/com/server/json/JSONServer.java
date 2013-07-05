326104434c692d919c3f940b9bbf9ffa2c9a337c
alvin-xu <479359285@qq.com>
2013-07-05 17:06:45 +0800

    json server success

--- src/com/server/json/JSONServer.java
+++ src/com/server/json/JSONServer.java
@@ -24,18 +24,18 @@ public class JSONServer extends HttpServlet{
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		super.doGet(req, resp);
 		System.out.println("get:"+req.getParameter("param"));
-		resp.getOutputStream().print("OK");
+		resp.getOutputStream().print("<html>OK</html>");
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		super.doPost(req, resp);
 		// 设置响应的数据种类类型
-        resp.setContentType("text/plain");
+		System.out.println("enter post");
+        
+		resp.setContentType("text/plain");
         // 设置编码为UTF-8
         resp.setCharacterEncoding("UTF-8");
         // 获取一个写对象，用于数据输出
@@ -50,6 +50,7 @@ public class JSONServer extends HttpServlet{
             // 将读取到的数据存放到缓存区里面
             sb.append(s);
         }
+        System.out.println("data: "+sb.toString());
         // 将字符数据转换成JSON数据对象
         JSONObject data = JSONObject.fromObject(sb.toString());
         // 获取请求数据里面的account
@@ -61,15 +62,20 @@ public class JSONServer extends HttpServlet{
         
         
         // 判断数据库查询结果是否为空，若是，则表示数据库不存在该用户
-        if (account==1 && password=="123") {
+        if (account==1 && password.equals("123")) {
             //将数据库的数据存放到JSONObject里面，便于发送给客户端
             data.put("user", "xubinbin");
             //往JSONObject里面存放一个boolean值true表示成功
             data.put("result", true);
+            
+            System.out.println("login success");
         }
         else{//如果不存在，则存放一个boolean值false表示失败
             data.put("result", false);
+            System.out.println("login fail");
         }
+        
+        System.out.println("end handle");
         //将JSONObject发送返回
         writer.write(data.toString());
         writer.flush();
